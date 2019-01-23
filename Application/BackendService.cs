158b8972008eb8a5395b258fa8f49f300ac9a364
John Ademola <crownedprinz@live.com>
2019-01-23 13:13:21 +0100

    Add project files.

---
+++ Application/BackendService.cs
@@ -0,0 +1,28 @@
+﻿//////////////////////////////////////////////////////////////////
+//
+//   PROGRAMMING ASP.NET CORE
+//   Dino Esposito
+//   
+//   EXTRAS 
+//   SignalR group notifications
+//  
+
+using SignalFriend.Backend;
+using SignalFriend.Models;
+
+namespace SignalFriend.Application
+{
+    public class BackendService
+    {
+        public static IndexViewModel GetUserModel(string currentUser)
+        {
+            var users = UserRepository.FriendsOf(currentUser);
+            var model = new IndexViewModel("")
+            {
+                Friends = users,
+                CurrentUser = currentUser
+            };
+            return model;
+        }
+    }
+}
---
+++ Models/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿//////////////////////////////////////////////////////////////////
+//
+//   PROGRAMMING ASP.NET CORE
+//   Dino Esposito
+//   
+//   EXTRAS 
+//   SignalR group notifications
+// 
+
+namespace SignalFriend.Models
+{
+    public class ViewModelBase
+    {
+        public ViewModelBase(string title = "")
+        {
+            Title = title;
+            ErrorMessage = "";
+            StatusCode = 0;
+            CurrentUser = "";
+        }
+
+        public string Title { get; set; }
+        public string ErrorMessage { get; set; }
+        public int StatusCode { get; set; }
+        public string CurrentUser { get; set; }
+    }
+}
